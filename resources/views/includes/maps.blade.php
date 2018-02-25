7a95d5f05da1faff0a2243d6cf55382854dd0349
Vasil Vasilev <vasco1494@gmail.com>
2018-02-25 14:32:15 +0200

    UPDATES

--- resources/views/includes/maps.blade.php
+++ resources/views/includes/maps.blade.php
@@ -23,61 +23,61 @@
                             <div class="card-body ">
                                 <div id="map" class="map">
                                 <script>
-                                    var map;
-                                    function initMap() {
-                                        map = new google.maps.Map(document.getElementById('map'), {
-                                        zoom: 2,
-                                        center: new google.maps.LatLng(2.8,-187.3),
-                                        mapTypeId: 'terrain'
-                                        });
+                                    var locations = [
+                                        ['loan 1', 33.890542, 151.274856, 'address 1'],
+                                        ['loan 2', 33.923036, 151.259052, 'address 2'],
+                                        ['loan 3', 34.028249, 151.157507, 'address 3'],
+                                        ['loan 4', 33.80010128657071, 151.28747820854187, 'address 4'],
+                                        ['loan 5', 33.950198, 151.259302, 'address 5']
+                                        ];
 
-                                        // Create a <script> tag and set the USGS URL as the source.
-                                        var script = document.createElement('script');
-                                        // This example uses a local copy of the GeoJSON stored at
-                                        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-                                        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-                                        document.getElementsByTagName('head')[0].appendChild(script);
+                                    function initialize() {
+
+                                        var myOptions = {
+                                        center: new google.maps.LatLng(33.890542, 151.274856),
+                                        zoom: 8,
+                                        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+                                        };
+                                        var map = new google.maps.Map(document.getElementById("default"),
+                                            myOptions);
+
+                                        setMarkers(map,locations)
+
                                     }
 
-                                    // Loop through the results array and place a marker for each
-                                    // set of coordinates.
-                                    window.eqfeed_callback = function(results) {
-                                        for (var i = 0; i < results.features.length; i++) {
-                                        var coords = results.features[i].geometry.coordinates;
-                                        var latLng = new google.maps.LatLng(coords[1],coords[0]);
-                                        var contentString = '<div id="content">'+
-                                        '<div id="siteNotice">'+
-                                        '</div>'+
-                                        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-                                        '<div id="bodyContent">'+
-                                        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                                        'sandstone rock formation in the southern part of the '+
-                                        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-                                        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-                                        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-                                        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-                                        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-                                        'Aboriginal people of the area. It has many springs, waterholes, '+
-                                        'rock caves and ancient paintings. Uluru is listed as a World '+
-                                        'Heritage Site.</p>'+
-                                        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-                                        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-                                        '(last visited June 22, 2009).</p>'+
-                                        '</div>'+
-                                        '</div>';
+                                    function setMarkers(map,locations){
 
-                                         var infowindow = new google.maps.InfoWindow({
-                                            content: contentString
-                                        });
-                                        var marker = new google.maps.Marker({
-                                            position: latLng,
-                                            map: map,
-                                            title: 'Uluru (Ayers Rock)'
-                                        });
-                                        marker.addListener('click', function() {
-                                            infowindow.open(map, marker);
-                                        });
-                                        }
+                                        var marker, i
+
+                                    for (i = 0; i < locations.length; i++)
+                                    {  
+
+                                    var loan = locations[i][0]
+                                    var lat = locations[i][1]
+                                    var long = locations[i][2]
+                                    var add =  locations[i][3]
+
+                                    latlngset = new google.maps.LatLng(lat, long);
+
+                                    var marker = new google.maps.Marker({  
+                                            map: map, title: loan , position: latlngset  
+                                            });
+                                            map.setCenter(marker.getPosition())
+
+
+                                            var content = "Loan Number: " + loan +  '</h3>' + "Address: " + add     
+
+                                    var infowindow = new google.maps.InfoWindow()
+
+                                    google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+                                            return function() {
+                                            infowindow.setContent(content);
+                                            infowindow.open(map,marker);
+                                            };
+                                        })(marker,content,infowindow)); 
+
+                                    }
                                     }
                                 </script>
                                 </div>
