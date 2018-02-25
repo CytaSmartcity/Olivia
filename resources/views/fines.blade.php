f90296e78622c28b1db0fe7d6e8fbdc3b19454cf
Chrysanthos Prodromou <pchrysanthos@gmail.com>
2018-02-25 15:16:27 +0200

    Add fines and bug fixes

---
+++ resources/views/fines.blade.php
@@ -0,0 +1,103 @@
+@include('includes.header')
+
+<body class="">
+<div class="wrapper ">
+    <div class="sidebar" data-color="orange">
+        <!--
+    Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+-->
+        <div class="logo">
+            <a href="https://www.facebook.com/Olivia-221586811741271/" class="simple-text logo-normal">
+                Olivia
+            </a>
+        </div>
+        @include('includes.sidebar')
+    </div>
+    <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent  navbar-absolute bg-primary fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-wrapper">
+                    <div class="navbar-toggle">
+                        <button type="button" class="navbar-toggler">
+                            <span class="navbar-toggler-bar bar1"></span>
+                            <span class="navbar-toggler-bar bar2"></span>
+                            <span class="navbar-toggler-bar bar3"></span>
+                        </button>
+                    </div>
+                    <a class="navbar-brand" href="#pablo">Fines</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                        aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar navbar-kebab"></span>
+                    <span class="navbar-toggler-bar navbar-kebab"></span>
+                    <span class="navbar-toggler-bar navbar-kebab"></span>
+                </button>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="panel-header panel-header-sm">
+        </div>
+        <div class="content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title"> {{ $title }}</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class=" text-primary">
+                                    <th>
+                                        ID
+                                    </th>
+                                    <th>
+                                        Amount
+                                    </th>
+                                    <th>
+                                        Reason
+                                    </th>
+                                    <th class="text-right">
+                                        Paid
+                                    </th>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($fines as $fine)
+                                        <tr>
+                                            <td>
+                                                {{ $fine->cy_id }}
+                                            </td>
+                                            <td>
+                                                {{ $fine->cost }}€
+                                            </td>
+                                            <td>
+                                                {{ $fine->reason }}
+                                            </td>
+                                            <td class="text-right">
+                                                {{ $fine->paid ? 'Yes' : 'No' }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="copyright">
+                    &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+</body>
+@include('includes.footer')
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use App\Classes\BOC;
 use App\Complain;
 use GuzzleHttp\Client;
 
@@ -22,7 +23,12 @@ Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
 
 Route::resource('complains', 'ComplainController');
+Route::resource('fines', 'FineController');
 Route::get('/pay', function() {
+    $client       = new Client();
+    $access_token = BOC::getAccessToken($client);
+
+    $code = 'AAI0EUKmh4DUQxe_MNmoO4mRLjzUkZYiSetYv8bEdZ6viKEpoIa9l19P0kU3ngqDxzr1T2piQpNQ2jFGKYvLgESUzkKVWOjXYjj4P_Il6OFuAQ';
 
     return view('includes.payments');
 });
@@ -31,38 +37,31 @@ Route::get('/map', function() {
     $fuel = \DB::table('fuel_records')->get();
 
     return view('includes.maps', compact('fuel'));
-});
+})->name('map');
 
-Route::get('/test', function() {
-
+Route::get('/login', function() {
     $client = new Client();
 
-    $headers = [
-        'accNumber'      => '351012345671',
-        'subscriptionId' => 'Subid000001 - 1519548222951',
-        'url'            => 'https=>//sandbox-apis.bankofcyprus.com/df-boc-org-sb/sb/psd2',
-        'client_id'      => 'dc25aaa9 - 9ba3 - 48ef - a253 - 22cc48c0569f',
-        'client_secret'  => 'wQ0eX4xU1lH8qN7cI1mG3oJ6sH6sH4yW0fW3nO0hH2rH2iE1yT',
-        'tppId'          => 'singpaymentdata',
-        'urlother'       => 'https=>//sandbox-apis.bankofcyprus.com/df-boc-org-sb/sb',
-        'PmtId'          => '',
-        'subscription2'  => '',
-        'originUserId'   => '50520218',
-        'oauthCode'      => 'AAIkZGMyNWFhYTktOWJhMy00OGVmLWEyNTMtMjJjYzQ4YzA1NjlmF6NZPlw5LEOWPxpMBe74-EKnNkmmjIHNYZrMzQfhDQwhqjtVBRTjboIK9QnQfpmUh8C8Qvk6JGgh9IqN99IJkNFFLXct9fdMYZFe676NJqWYTjlRs5HW5PK0cSuv38KdVb8IDek9skF8Z56lsG0ITg',
-        'journeyId'      => 'abc',
-        'oauthCode2'     => ',',
-    ];
+    $client_id = 'dc25aaa9-9ba3-48ef-a253-22cc48c0569f';
+    //$client_secret = 'wQ0eX4xU1lH8qN7cI1mG3oJ6sH6sH4yW0fW3nO0hH2rH2iE1yT';
 
-    $body     = [
-        'client_id'     => 'dc25aaa9-9ba3-48ef-a253-22cc48c0569f',//env('BOC_CLIENT_ID'),
-        'client_secret' => 'wQ0eX4xU1lH8qN7cI1mG3oJ6sH6sH4yW0fW3nO0hH2rH2iE1yT',//env('BOC_APP_SECRET'),
-        'grant_type'    => 'client_credentials',
-        'scope'         => 'TTPOAuth2Security',
-    ];
-    $response = $client->post('https://sandbox-apis.bankofcyprus.com/df-boc-org-sb/sb/psd2/oauth2/token', [
-        'headers' => $headers,
-        'json'    => $body,
-    ]);
+    $access_token = BOC::getAccessToken($client);
 
-    return $response->getBody()->getContents();
+    $subscription_id = BOC::createSubscriptions($client, $access_token);
+
+    return redirect()->away('https://sandbox.bankofcyprus.com/sandbox/login?original-url=https%3A%2F%2Fsandbox-apis.bankofcyprus.com%2Fdf-boc-org-sb%2Fsb%2Fpsd2%2Foauth2%2Fauthorize%3Fresponse_type%3Dcode%26amp%3Bredirect_uri%3Dhttps%3A%2F%2Folivia-cyta.herokuapp.com%2F%26scope%3DUserOAuth2Security%26client_id%3D'.$client_id.'%26subscriptionid%3D'.$subscription_id.'%26rstate%3DP3WDKMoYgGpZICLtPR_8VVbm_u5JfRmP5Jswqf88UU8&app-name=Olivia&appid=5a926efee4b0932a8d3ea78e&org=chrysanthos-prodromou&orgid=5a926e2ee4b0932a8d3ea780&provider=df-boc-org-sb&providerid=5a439af0e4b07064433ef6e4&catalog=sb&catalogid=5a439af0e4b07064433ef6f0&g-transid=6f100acb5a927813048ef3c1&transid=76477377');
+
 });
+
+Route::get('/verify', function() {
+
+    $verify = BOC::verifyCode();
+
+    return $verify;
+});
+
+Route::get('/pay', function() {
+
+    return view('includes.login');
+});
+
